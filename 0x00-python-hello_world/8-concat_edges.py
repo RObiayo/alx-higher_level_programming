b2a597720abb391ef9caa0ea55e7f1e257d811e1
henryotieno <henrynyobange@gmail.com>
2023-06-05 01:21:58 -0700

    Beginning of Python

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is a simplified, friendlier, object-oriented programming\
+        language that has great usability with an understandable syntax"
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
