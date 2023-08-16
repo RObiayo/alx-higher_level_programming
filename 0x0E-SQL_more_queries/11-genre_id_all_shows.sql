244e8acf0c7e3342cd99813fb7d471e828ccdb16
RObiayo <ronnieobiayo59@gmail.com>
2023-08-16 03:09:41 -0700

    SQL more Queries

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,8 @@
+-- Gives all shows contained in the db hbtn_0d_tvshows
+-- Shows NULL for shows without genres
+-- Data are ordered by ascending tv_shows.title and tv_show_genres.genre_id
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- List all cities in California
+-- Results in ascending order
+SELECT `id`, `name`
+  FROM `cities`
+ WHERE `state_id` IN
+       (SELECT `id`
+	  FROM `states`
+	 WHERE `name` = "California")
+ ORDER BY `id`;
